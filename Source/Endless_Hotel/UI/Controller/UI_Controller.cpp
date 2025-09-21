d17b43c68f471a5000ce87def7babb4d202fec9b
KaneBigNose <imddongs@naver.com>
2025-09-21 20:45:28 +0900

    Feat: UI Controller 추가

--- Source/Endless_Hotel/UI/Controller/UI_Controller.cpp
+++ Source/Endless_Hotel/UI/Controller/UI_Controller.cpp
@@ -1,3 +1,159 @@
 ﻿// Copyright by 2025-2 WAP Game 2 team
 
 #include "UI/Controller/UI_Controller.h"
+#include "UI/PopUp/UI_PopUp_Base.h"
+#include "GameFramework/PlayerController.h"
+#include "Kismet/GameplayStatics.h"
+
+#pragma region Open
+
+UUI_Base* UUI_Controller::OpenBaseWidget(TSubclassOf<UUI_Base> WidgetClass, const EInputModeType& InputMode)
+{
+	if (BaseWidget)
+	{
+		CloseBaseWidget(InputMode);
+	}
+
+	BaseWidget = CreateWidget<UUI_Base>(GetWorld(), WidgetClass);
+	BaseWidget->AddToViewport(BASE_UI_ZORDER);
+
+	SetInputMode(InputMode);
+
+	return BaseWidget;
+}
+
+UUI_PopUp_Base* UUI_Controller::OpenPopUpWidget(TSubclassOf<UUI_PopUp_Base> WidgetClass, const EInputModeType& InputMode)
+{
+	if (PopUpWidget)
+	{
+		ClosePopUpWidget(InputMode);
+	}
+
+	PopUpWidget = CreateWidget<UUI_PopUp_Base>(GetWorld(), WidgetClass);
+	PopUpWidget->AddToViewport(POPUP_UI_ZORDER);
+
+	SetInputMode(InputMode);
+
+	return PopUpWidget;
+}
+
+UUI_PopUp_Base* UUI_Controller::OpenStrongPopUpWidget(TSubclassOf<UUI_PopUp_Base> WidgetClass, const EInputModeType& InputMode)
+{
+	if (PopUpWidget)
+	{
+		ClosePopUpWidget(InputMode);
+	}
+
+	PopUpWidget = CreateWidget<UUI_PopUp_Base>(GetWorld(), WidgetClass);
+	GEngine->GameViewport->AddViewportWidgetContent(PopUpWidget->TakeWidget(), POPUP_UI_ZORDER);
+
+	SetInputMode(InputMode);
+
+	return PopUpWidget;
+}
+
+#pragma endregion
+
+#pragma region Close
+
+void UUI_Controller::CloseBaseWidget(const EInputModeType& InputMode)
+{
+	if (!BaseWidget)
+	{
+		return;
+	}
+
+	BaseWidget->RemoveFromViewport();
+	BaseWidget = nullptr;
+
+	SetInputMode(InputMode);
+}
+
+void UUI_Controller::ClosePopUpWidget(const EInputModeType& InputMode)
+{
+	if (!PopUpWidget)
+	{
+		return;
+	}
+
+	PopUpWidget->RemoveFromViewport();
+	PopUpWidget = nullptr;
+
+	SetInputMode(InputMode);
+}
+
+void UUI_Controller::CloseStrongPopUpWidget(const EInputModeType& InputMode)
+{
+	if (!PopUpWidget)
+	{
+		return;
+	}
+
+	GEngine->GameViewport->RemoveViewportWidgetContent(PopUpWidget->TakeWidget());
+	PopUpWidget = nullptr;
+
+	SetInputMode(InputMode);
+}
+
+#pragma endregion
+
+#pragma region Input
+
+void UUI_Controller::SetInputMode(const EInputModeType& InputMode)
+{
+	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+
+	switch (InputMode)
+	{
+	case EInputModeType::GameOnly:
+	{
+		if (PopUpWidget)
+		{
+			return;
+		}
+
+		PC->SetInputMode(FInputModeGameOnly());
+		PC->bShowMouseCursor = false;
+
+		break;
+	}
+	case EInputModeType::UIOnly:
+	{
+		FInputModeUIOnly InputMode;
+
+		if (!PopUpWidget)
+		{
+			InputMode.SetWidgetToFocus(BaseWidget->TakeWidget());
+		}
+		else
+		{
+			InputMode.SetWidgetToFocus(PopUpWidget->TakeWidget());
+		}
+
+		PC->SetInputMode(InputMode);
+		PC->bShowMouseCursor = true;
+
+		break;
+	}
+	case EInputModeType::GameAndUI:
+	{
+		FInputModeGameAndUI InputMode;
+
+		if (!PopUpWidget)
+		{
+			InputMode.SetWidgetToFocus(BaseWidget->TakeWidget());
+		}
+		else
+		{
+			InputMode.SetWidgetToFocus(PopUpWidget->TakeWidget());
+		}
+
+		PC->SetInputMode(InputMode);
+		PC->bShowMouseCursor = true;
+
+		break;
+	}
+	}
+}
+
+#pragma endregion
--- Source/Endless_Hotel/UI/Controller/UI_Controller.h
+++ Source/Endless_Hotel/UI/Controller/UI_Controller.h
@@ -6,9 +6,50 @@
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "UI_Controller.generated.h"
 
+#pragma region Declare
+
+#define BASE_UI_ZORDER 0
+#define POPUP_UI_ZORDER 1
+
+UENUM(BlueprintType)
+enum class EInputModeType : uint8
+{
+	GameOnly	UMETA(DisplayName = "GameOnly"),
+	UIOnly		UMETA(DisplayName = "UIOnly"),
+	GameAndUI	UMETA(DisplayName = "GameAndUI")
+};
+
+#pragma endregion
+
 UCLASS()
 class ENDLESS_HOTEL_API UUI_Controller : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
-	
+
+#pragma region Widget
+
+public:
+	class UUI_Base* OpenBaseWidget(TSubclassOf<class UUI_Base> WidgetClass, const EInputModeType& InputMode = EInputModeType::UIOnly);
+	class UUI_PopUp_Base* OpenPopUpWidget(TSubclassOf<class UUI_PopUp_Base> WidgetClass, const EInputModeType& InputMode = EInputModeType::UIOnly);
+	class UUI_PopUp_Base* OpenStrongPopUpWidget(TSubclassOf<class UUI_PopUp_Base> WidgetClass, const EInputModeType& InputMode = EInputModeType::UIOnly);
+
+	void CloseBaseWidget(const EInputModeType& InputMode = EInputModeType::GameOnly);
+	void ClosePopUpWidget(const EInputModeType& InputMode = EInputModeType::GameOnly);
+	void CloseStrongPopUpWidget(const EInputModeType& InputMode = EInputModeType::GameOnly);
+
+	class UUI_Base* GetCurrentBaseWidget() { return BaseWidget; }
+	class UUI_PopUp_Base* GetCurrentPopUpWidget() { return PopUpWidget; }
+
+protected:
+	void SetInputMode(const EInputModeType& InputMode);
+
+protected:
+	UPROPERTY()
+	TObjectPtr<class UUI_Base> BaseWidget;
+
+	UPROPERTY()
+	TObjectPtr<class UUI_PopUp_Base> PopUpWidget;
+
+#pragma endregion
+
 };
