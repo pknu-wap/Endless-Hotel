b0909f48328337b6678f7801fbf015fb0cb082d5
KaneBigNose <imddongs@naver.com>
2025-09-25 17:37:53 +0900

    Feat: Setting UI 추가 및 Save 시스템 구현

---
+++ Source/Endless_Hotel/GameSystem/SaveGame/SaveManager.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "GameSystem/SaveGame/SaveManager.h"
+#include "Kismet/GameplayStatics.h"
+
+#pragma region Setting
+
+FSettingSaveData USaveManager::LoadSettingData()
+{
+	USaveManager* SaveManager = Cast<USaveManager>(UGameplayStatics::LoadGameFromSlot(TEXT("Save_Setting"), 0));
+	FSettingSaveData SaveData;
+
+	if (!SaveManager)
+	{
+		return SaveData;
+	}
+
+	SaveData = SaveManager->SettingData;
+
+	return SaveData;
+}
+
+void USaveManager::SaveSettingData(const FSettingSaveData& Data)
+{
+	USaveManager* SaveManager = Cast<USaveManager>(UGameplayStatics::CreateSaveGameObject(USaveManager::StaticClass()));
+	SaveManager->SettingData = Data;
+
+	UGameplayStatics::SaveGameToSlot(SaveManager, TEXT("Save_Setting"), 0);
+}
+
+#pragma endregion
---
+++ Source/Endless_Hotel/UI/Button/Setting/UI_Button_Setting.h
@@ -0,0 +1,66 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Button/UI_Button_Base.h"
+#include "UI_Button_Setting.generated.h"
+
+#pragma region Declare
+
+UENUM(BlueprintType)
+enum class ESettingCategory : uint8
+{
+	Grapic		UMETA(DisplayName = "Grapic"),
+	Language	UMETA(DisplayName = "Language"),
+	Resolution	UMETA(DisplayName = "Resolution"),
+	Screen		UMETA(DisplayName = "Screen"),
+	Sound		UMETA(DisplayName = "Sound")
+};
+
+USTRUCT(BlueprintType)
+struct FButtonInfo
+{
+	GENERATED_BODY()
+
+	ESettingCategory Category;
+	int32 Highlight_Index;
+
+	int32 Value_Int;
+	FString Value_String;
+	FIntPoint Value_IntPoint;
+	EWindowMode::Type Value_WindowMode;
+	float Value_Float;
+};
+
+#pragma endregion
+
+UCLASS()
+class ENDLESS_HOTEL_API UUI_Button_Setting : public UUI_Button_Base
+{
+	GENERATED_BODY()
+
+#pragma region Base
+
+public:
+	UUI_Button_Setting();
+
+#pragma endregion
+	
+#pragma region Setting
+
+public:
+	UPROPERTY(EditAnywhere)
+	FButtonInfo ButtonInfo;
+
+#pragma endregion
+
+#pragma region Click
+
+protected:
+	UFUNCTION()
+	void ButtonClick();
+
+#pragma endregion
+
+};
