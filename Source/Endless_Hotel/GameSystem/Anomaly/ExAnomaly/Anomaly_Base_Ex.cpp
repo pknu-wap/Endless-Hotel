21353cebc4b4dc82aaba776c9e812028a8b4322c
kwkim526 <polops2331@gmail.com>
2025-09-30 18:36:26 +0900

    Feat: 이상현상 발생 구현
    이상현상 발생 구현

---
+++ Source/Endless_Hotel/GameSystem/Anomaly/ExAnomaly/Anomaly_Base_Ex.cpp
@@ -0,0 +1,27 @@
+// Copyright by 2025-2 WAP Game 2 team
+
+
+#include "Anomaly_Base_Ex.h"
+
+// Sets default values
+AAnomaly_Base_Ex::AAnomaly_Base_Ex(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+ 	// Don't use Tick
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void AAnomaly_Base_Ex::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAnomaly_Base_Ex::StartAnomaly()
+{
+	UE_LOG(LogTemp, Log, TEXT("Anomaly %d Started"), AnomalyID);
+}
+void AAnomaly_Base_Ex::StartAnomaly_Implementation()
+{
+	// Default implementation does nothing
+}
