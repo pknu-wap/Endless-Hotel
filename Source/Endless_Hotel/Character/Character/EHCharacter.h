94912545ad0676dcb8d212a27b23c58fbcf15ac9
KaneBigNose <imddongs@naver.com>
2025-10-06 17:34:01 +0900

    Feat: 눈 뜨는 연출 효과 추가
    Feat: 플레이어 클래스 추가 및 테스트 플레이어 블루프린트 추가

---
+++ Source/Endless_Hotel/Character/Character/EHCharacter.h
@@ -0,0 +1,21 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "EHCharacter.generated.h"
+
+UCLASS()
+class ENDLESS_HOTEL_API AEHCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+#pragma region Base
+
+public:
+	AEHCharacter(const FObjectInitializer& ObjectInitializer);
+
+#pragma endregion
+
+};
---
+++ Source/Endless_Hotel/Player/Character/EHPlayer.cpp
@@ -0,0 +1,14 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "Player/Character/EHPlayer.h"
+#include "Player/Component/EHCameraComponent.h"
+
+#pragma region Base
+
+AEHPlayer::AEHPlayer(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	Component_Camera = CreateDefaultSubobject<UEHCameraComponent>(TEXT("Component_Camera"));
+}
+
+#pragma endregion
