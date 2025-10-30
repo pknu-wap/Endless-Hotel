efc421d2ad9dc6adc4b319ecffaaccdfbf9951e5
KaneBigNose <imddongs@naver.com>
2025-10-31 03:53:54 +0900

    Feat: 우는 소리 이상현상 추가
    현재 문 두드리는 이상현상의 사운드가 출력되지 않는 현상이 있음 (버그)

---
+++ Source/Endless_Hotel/Anomaly/EightExit/Cry/Anomaly_Cry.cpp
@@ -0,0 +1,37 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "Anomaly/EightExit/Cry/Anomaly_Cry.h"
+#include "Components/AudioComponent.h"
+
+#pragma region Base
+
+AAnomaly_Cry::AAnomaly_Cry(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	AC = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AC->OnAudioFinished.AddDynamic(this, &ThisClass::PlayCrySound);
+}
+
+#pragma endregion
+
+#pragma region Activity
+
+void AAnomaly_Cry::ActivateAnomaly_Implementation(uint8 Anomaly_ID)
+{
+	Super::ActivateAnomaly_Implementation(Anomaly_ID);
+
+	FTimerHandle SoundHandle;
+	GetWorld()->GetTimerManager().SetTimer(SoundHandle, this, &ThisClass::PlayCrySound, 10, false);
+}
+
+#pragma endregion
+
+#pragma region Cry
+
+void AAnomaly_Cry::PlayCrySound()
+{
+	AC->Sound = Sound_Cry;
+	AC->Play();
+}
+
+#pragma endregion
