8f17f497b58bd8ec9d7a1e08116c261344e65037
KaneBigNose <imddongs@naver.com>
2025-11-06 01:07:42 +0900

    Feat: 시야흐리기 현상 추가

---
+++ Source/Endless_Hotel/Anomaly/EightExit/Blur/Anomaly_Blur.cpp
@@ -0,0 +1,41 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "Anomaly/EightExit/Blur/Anomaly_Blur.h"
+#include "UI/Controller/UI_Controller.h"
+#include "Components/AudioComponent.h"
+
+#pragma region Base
+
+AAnomaly_Blur::AAnomaly_Blur(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	AC = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AC->SetupAttachment(RootComponent);
+}
+
+#pragma endregion
+
+#pragma region Activity
+
+void AAnomaly_Blur::ActivateAnomaly_Implementation(uint8 Anomaly_ID)
+{
+	Super::ActivateAnomaly_Implementation(Anomaly_ID);
+
+	FTimerHandle StartHandle;
+	GetWorld()->GetTimerManager().SetTimer(StartHandle, this, &ThisClass::ShowBlurWiget, 10, false);
+}
+
+#pragma endregion
+
+#pragma region Blur
+
+void AAnomaly_Blur::ShowBlurWiget()
+{
+	UUI_Controller* UICon = GetGameInstance()->GetSubsystem<UUI_Controller>();
+	UICon->OpenPopUpWidget(UI_Blur, EInputModeType::GameOnly);
+
+	AC->Sound = Sound_Blur;
+	AC->Play();
+}
+
+#pragma endregion
---
+++ Source/Endless_Hotel/UI/PopUp/Default/Blur/UI_PopUp_Blur.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "UI/PopUp/Default/Blur/UI_PopUp_Blur.h"
+#include "Components/BackgroundBlur.h"
+
+#pragma region Base
+
+void UUI_PopUp_Blur::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	StartSightBlur();
+}
+
+#pragma endregion
+
+#pragma region Blur
+
+void UUI_PopUp_Blur::StartSightBlur()
+{
+	const float TargetStrength = 20.f;
+	float CurrentStrength = 0.f;
+
+	GetWorld()->GetTimerManager().SetTimer(BlurHandle, [this, TargetStrength, CurrentStrength]() mutable
+		{
+			CurrentStrength += 0.1f;
+			BackBlur->SetBlurStrength(CurrentStrength);
+
+			if (CurrentStrength >= TargetStrength)
+			{
+				GetWorld()->GetTimerManager().ClearTimer(BlurHandle);
+			}
+		}, 0.01f, true);
+}
+
+#pragma endregion
