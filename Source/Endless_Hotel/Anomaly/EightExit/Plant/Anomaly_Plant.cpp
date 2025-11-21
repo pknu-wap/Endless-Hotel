683631773abf2a43d4a8c5bea03baeeb6f06cb33
ssiver6324 <ssiver6324@gmail.com>
2025-11-21 14:55:25 +0900

    Chore: 호실 머티리얼 추가
    호실 팻말 머티리얼 추가

--- Source/Endless_Hotel/Anomaly/EightExit/Plant/Anomaly_Plant.cpp
+++ Source/Endless_Hotel/Anomaly/EightExit/Plant/Anomaly_Plant.cpp
@@ -2,4 +2,20 @@
 
 
 #include "Anomaly/EightExit/Plant/Anomaly_Plant.h"
-#include "Kismet/GameplayStatics.h"
+#include "Anomaly/Object/Plant/Anomaly_Object_Plant.h"
+
+#pragma region Activity
+
+void AAnomaly_Plant::ActivateAnomaly_Implementation(uint8 Anomaly_ID)
+{
+
+	switch (Anomaly_ID)
+	{
+	case 6:
+		AnomalyAction = ([](AAnomaly_Object_Base* AnomalyObject)
+			{
+			});
+		break;
+	}
+
+}
--- Source/Endless_Hotel/Anomaly/EightExit/Plant/Anomaly_Plant.h
+++ Source/Endless_Hotel/Anomaly/EightExit/Plant/Anomaly_Plant.h
@@ -6,12 +6,16 @@
 #include "Anomaly/Base/Anomaly_Base_EightExit.h"
 #include "Anomaly_Plant.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class ENDLESS_HOTEL_API AAnomaly_Plant : public AAnomaly_Base_EightExit
 {
 	GENERATED_BODY()
-	
+
+#pragma region Activity
+
+public:
+	virtual void ActivateAnomaly_Implementation(uint8 Anomaly_ID) override;
+
+#pragma endregion
+
 };
