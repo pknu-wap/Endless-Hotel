903c6a85bb06eed10e179a34357744b7186d080f
ssiver6324 <ssiver6324@gmail.com>
2026-01-01 01:19:08 +0900

    Feature: 인형C++구현
    상호작용 구현X

--- Source/Endless_Hotel/Anomaly/Object/ShelfDoll/Anomaly_Object_ShelfDoll.cpp
+++ Source/Endless_Hotel/Anomaly/Object/ShelfDoll/Anomaly_Object_ShelfDoll.cpp
@@ -2,4 +2,47 @@
 
 
 #include "Anomaly/Object/ShelfDoll/Anomaly_Object_ShelfDoll.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/Actor.h"
 
+#pragma region Base
+
+AAnomaly_Object_ShelfDoll::AAnomaly_Object_ShelfDoll(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	SM_Doll = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM_Doll"));
+	SM_Doll->SetupAttachment(RootComponent);
+
+	SM_Doll->SetVisibility(false);
+	SM_Doll->SetHiddenInGame(true);
+}
+
+#pragma endregion
+
+#pragma region Doll
+
+void AAnomaly_Object_ShelfDoll::ActivateDoll_Show()
+{
+	if (!SM_Doll)
+	{
+		return;
+	}
+
+	SM_Doll->SetVisibility(true);
+	SM_Doll->SetHiddenInGame(false);
+}
+
+#pragma endregion
+
+#pragma region ShelfBook
+
+void AAnomaly_Object_ShelfDoll::ActivateShelfBook_Hide()
+{
+	if (!BP_ShelfBookActor)
+	{
+		return;
+	}
+	BP_ShelfBookActor->SetActorHiddenInGame(true);
+}
+
+#pragma endregion
