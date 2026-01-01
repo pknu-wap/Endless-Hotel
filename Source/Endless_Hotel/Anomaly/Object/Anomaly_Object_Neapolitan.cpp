9beb65f36e680aba0aa3f92842f44fa5df8e697c
kwkim526 <polops2331@gmail.com>
2026-01-01 16:53:43 +0900

    Feat: 나폴리탄 오브젝트 스크립트 추가
    -나폴리탄 이상현상 전용 오브젝트 스크립트 추가

---
+++ Source/Endless_Hotel/Anomaly/Object/Anomaly_Object_Neapolitan.cpp
@@ -0,0 +1,49 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+
+#include "Anomaly/Object/Anomaly_Object_Neapolitan.h"
+#include "Components/WidgetComponent.h"
+#include "UI/Base/InGame/Interact/UI_Interact.h"
+#include "Component/LookAt/LookAtComponent.h"
+
+#define LOCTEXT_NAMESPACE "Anomaly_Object_Neapolitan"
+
+#pragma region Base
+
+
+AAnomaly_Object_Neapolitan::AAnomaly_Object_Neapolitan(const FObjectInitializer& ObjectInitializer)
+{
+	Object = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Object"));
+	Object->SetupAttachment(RootComponent);
+	WC = CreateDefaultSubobject<UWidgetComponent>(TEXT("WC"));
+	WC->SetupAttachment(Object);
+	LAC = CreateDefaultSubobject<ULookAtComponent>(TEXT("LAC"));
+}
+
+void AAnomaly_Object_Neapolitan::BeginPlay()
+{
+	Super::BeginPlay();
+	bSolved = false;
+	if (!WC) return;
+	UI_Interact = Cast<UUI_Interact>(WC->GetUserWidgetObject());
+	UI_Interact->SetDescription(LOCTEXT("Key1", "상호작용"));
+	LAC->SettingWidgetComponent(WC);
+}
+
+#pragma endregion
+
+#undef LOCTEXT_NAMESPACE
+
+#pragma region Interact
+
+void AAnomaly_Object_Neapolitan::Interacted_Implementation()
+{
+	bSolved = !bSolved;
+}
+
+void AAnomaly_Object_Neapolitan::ShowInteractWidget_Implementation(bool bIsShow)
+{
+	UI_Interact->ShowDescription(bIsShow);
+}
+
+#pragma endregion
---
+++ Source/Endless_Hotel/Anomaly/Object/Anomaly_Object_Neapolitan.h
@@ -0,0 +1,58 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include <CoreMinimal.h>
+#include "Anomaly/Object/Anomaly_Object_Base.h"
+#include "Interact/InteractableObject.h"
+#include "Anomaly_Object_Neapolitan.generated.h"
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Object_Neapolitan : public AAnomaly_Object_Base, public IInteractableObject
+{
+	GENERATED_BODY()
+	
+#pragma region Base
+
+public:
+	AAnomaly_Object_Neapolitan(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual void BeginPlay() override;
+
+#pragma endregion
+
+#pragma region Object
+
+public:
+	UPROPERTY(EditAnywhere, Category = "AnomalyObject")
+	TObjectPtr<UStaticMeshComponent> Object;
+
+#pragma endregion
+
+#pragma region Verdict
+
+public:
+	bool bSolved;
+
+#pragma endregion
+
+#pragma region Interact
+
+public:
+	virtual void Interacted_Implementation() override;
+	virtual void ShowInteractWidget_Implementation(bool bIsShow) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<class UWidgetComponent> WC;
+
+	UPROPERTY()
+	TObjectPtr<class UUI_Interact> UI_Interact;
+
+	UPROPERTY()
+	TObjectPtr<class ULookAtComponent> LAC;
+
+#pragma endregion
+
+};
