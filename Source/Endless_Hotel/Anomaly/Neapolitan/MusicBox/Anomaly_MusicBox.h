d4d2dc45d6aa78ee21e08c558c092517f49ab9af
kwkim526 <polops2331@gmail.com>
2026-01-05 12:12:31 +0900

    Feat: 오르골 이상현상 추가
    -오르골 이상현상 추가

---
+++ Source/Endless_Hotel/Anomaly/Neapolitan/MusicBox/Anomaly_MusicBox.h
@@ -0,0 +1,21 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Anomaly/Base/Anomaly_Base_Neapolitan.h"
+#include <CoreMinimal.h>
+#include <Anomaly_MusicBox.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_MusicBox : public AAnomaly_Base_Neapolitan
+{
+	GENERATED_BODY()
+	
+#pragma region Activity
+
+public:
+	virtual void ActivateAnomaly(uint8 Anomaly_ID) override;
+
+#pragma endregion
+
+};
---
+++ Source/Endless_Hotel/Anomaly/Object/MusicBox/Anomaly_Object_MusicBox.h
@@ -0,0 +1,66 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Anomaly/Object/Anomaly_Object_Neapolitan.h"
+#include <CoreMinimal.h>
+#include <Anomaly_Object_MusicBox.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Object_MusicBox : public AAnomaly_Object_Neapolitan
+{
+	GENERATED_BODY()
+
+#pragma region Base
+
+public:
+	AAnomaly_Object_MusicBox(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "BoxBody")
+	TObjectPtr<class UStaticMeshComponent> Mesh_BoxBody;
+
+	UPROPERTY(EditAnywhere, Category = "BoxBody")
+	TObjectPtr<class UStaticMeshComponent> Mesh_BoxRotator;
+
+#pragma endregion
+
+#pragma region Sound
+
+protected:
+	UPROPERTY()
+	TObjectPtr<class UAudioComponent> AC;
+
+	UPROPERTY(EditAnywhere, Category = "Sound")
+	TObjectPtr<class USoundWave> Sound_MusicBox;
+
+#pragma endregion
+
+
+#pragma region PlayMusicBox
+
+public:
+	void PlayMusicBox();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "MusicBox|Fail");
+	FTimerHandle FailTimerHandle;
+
+	UPROPERTY(EditAnywhere, Category = "MusicBox|Fail");
+	float LimitTime;
+
+#pragma endregion
+
+
+#pragma region Interact
+
+protected:
+	virtual void Interacted_Implementation() override;
+
+protected:
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category = "MusicBox|Interact")
+	bool bWaitingInteract = false;
+
+#pragma endregion
+
+};
