86fed819b8887f2f23737da8c3c5778aeffe9c69
bumyee11 <gd132229@gmail.com>
2026-01-07 16:32:11 +0900

    Feat: 중력 이상현상 초안 제작
    물체가 무작위 방향으로 떠오르다 떨어지는 로직 제작

---
+++ Source/Endless_Hotel/Anomaly/Neapolitan/Float/Anomaly_Float.h
@@ -0,0 +1,22 @@
+// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Anomaly/Base/Anomaly_Base_Neapolitan.h"
+#include "Anomaly_Float.generated.h"
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Float : public AAnomaly_Base_Neapolitan
+{
+	GENERATED_BODY()
+
+
+#pragma region Activity
+
+public:
+	virtual void ActivateAnomaly(uint8 Anomaly_ID) override;
+
+#pragma endregion
+
+};
---
+++ Source/Endless_Hotel/Anomaly/Object/Float/Anomaly_Object_Float.h
@@ -0,0 +1,62 @@
+// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Anomaly/Object/Anomaly_Object_Neapolitan.h"
+#include "Anomaly_Object_Float.generated.h"
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Object_Float : public AAnomaly_Object_Neapolitan
+{
+	GENERATED_BODY()
+
+#pragma region Base
+
+public:
+    AAnomaly_Object_Float(const FObjectInitializer& ObjectInitializer);
+
+protected:
+    virtual void BeginPlay() override;
+
+#pragma endregion
+
+#pragma region Interact
+
+public:
+    virtual void Interacted_Implementation() override;
+
+#pragma endregion
+
+#pragma region Floating
+
+public:
+    void StartFloating();
+    void StopFloating();
+    void RestoreToOrigin();
+    void FloatAccelerate();
+
+protected:
+    FTimerHandle FloatTickTimer;
+    FTimerHandle StopFloatTimer;
+
+#pragma endregion
+
+#pragma region Object
+
+protected:
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent* Mesh;
+
+    FVector OriginLocation;
+    FRotator OriginRotation;
+
+    FVector FloatVelocity;
+    FRotator RotationVelocity;
+
+    bool bIsFloating = false;
+
+#pragma endregion
+
+
+};
