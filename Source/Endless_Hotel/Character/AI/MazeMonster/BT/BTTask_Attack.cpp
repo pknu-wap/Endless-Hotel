0375f96d8fa63e03bdc2c0d7cf49892b8a189975
kwkim526 <polops2331@gmail.com>
2026-01-19 16:39:59 +0900

    Feat: 귀신 점프스퀘어를 위한 애니메이션 추가
    -점프스퀘어용 애니메이션 추가

--- Source/Endless_Hotel/Character/AI/MazeMonster/BT/BTTask_Attack.cpp
+++ Source/Endless_Hotel/Character/AI/MazeMonster/BT/BTTask_Attack.cpp
@@ -1,5 +1,42 @@
-// Copyright by 2025-2 WAP Game 2 team
+﻿#include "Character/AI/MazeMonster/BT/BTTask_Attack.h"
+#include "Character/AI/BaseAIAnimInstance.h"
+#include "Player/Character/EHPlayer.h"
+#include "Character/AI/MazeMonster/MazeMonsterController.h"
+#include <AIController.h>
+#include <GameFramework/Character.h>
+#include <BehaviorTree/BlackboardComponent.h>
+
+#pragma region Base
+
+UBTTask_Attack::UBTTask_Attack()
+{
+	NodeName = TEXT("MazeMonster Attack");
+}
+
+#pragma endregion
+
+#pragma region Task
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	APawn* AIPawn = AIController->GetPawn();
+	ACharacter* Character = Cast<ACharacter>(AIPawn);
+	UBaseAIAnimInstance* AnimInst = Cast<UBaseAIAnimInstance>(Character->GetMesh()->GetAnimInstance());
+	AnimInst->State = EAIAnimState::Attacking;
 
 
-#include "Character/AI/MazeMonster/BT/BTTask_Attack.h"
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	UObject* TargetObject = BlackboardComp->GetValueAsObject(AMazeMonsterController::Key_TargetPlayer);
+
+	return EBTNodeResult::Succeeded;
+	return EBTNodeResult::Succeeded;
+
+	AEHPlayer* Player = Cast<AEHPlayer>(TargetObject);
+	Player->DieDelegate.Broadcast(EDeathReason::Attack);
+}
+
+#pragma endregion
 
