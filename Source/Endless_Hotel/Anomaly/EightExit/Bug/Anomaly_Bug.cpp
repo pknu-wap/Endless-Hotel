f7afba9d641c54e3ba23a46c6b9cd1ae65a7cd15
이상혁 <imddongs@naver.com>
2026-01-22 01:32:00 +0900

    Feat: 벌레 이상현상 추가

---
+++ Source/Endless_Hotel/Anomaly/EightExit/Bug/Anomaly_Bug.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "Anomaly/EightExit/Bug/Anomaly_Bug.h"
+#include <NiagaraFunctionLibrary.h>
+
+#pragma region Activity
+
+void AAnomaly_Bug::ActivateAnomaly()
+{
+	Super::ActivateAnomaly();
+
+	switch (AnomalyName)
+	{
+	case EAnomalyName::Bug:
+		AnomalyAction = ([this](AAnomaly_Object_Base* Object)
+			{
+				SpawnBugs();
+			});
+		ActiveTrigger();
+		break;
+	}
+}
+
+#pragma endregion
+
+#pragma region Bug
+
+void AAnomaly_Bug::SpawnBugs()
+{
+	for (FVector Target : BugLocations)
+	{
+		UNiagaraFunctionLibrary::SpawnSystemAtLocation(this, NS_Bug, Target);
+	}
+}
+
+#pragma endregion
---
+++ Source/Endless_Hotel/Anomaly/EightExit/Bug/Anomaly_Bug.h
@@ -0,0 +1,35 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Anomaly/Base/Anomaly_Base_EightExit.h"
+#include <CoreMinimal.h>
+#include <Anomaly_Bug.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Bug : public AAnomaly_Base_EightExit
+{
+	GENERATED_BODY()
+
+#pragma region Activity
+
+public:
+	virtual void ActivateAnomaly() override;
+
+#pragma endregion
+
+#pragma region Bug
+
+protected:
+	void SpawnBugs();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Bug")
+	TObjectPtr<class UNiagaraSystem> NS_Bug;
+
+	UPROPERTY(EditAnywhere, Category = "Bug")
+	TArray<FVector> BugLocations;
+
+#pragma endregion
+
+};
