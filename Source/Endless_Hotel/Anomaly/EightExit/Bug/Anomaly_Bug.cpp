55fb9d62465606a8c86d11293528b4096af101a0
kwkim526 <polops2331@gmail.com>
2026-01-23 23:47:31 +0900

    Fixing: 머지 오류 수정중
    -머지 오류 생김 수정중

---
+++ Source/Endless_Hotel/Anomaly/EightExit/Bug/Anomaly_Bug.cpp
@@ -0,0 +1,38 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#include "Anomaly/EightExit/Bug/Anomaly_Bug.h"
+#include "Anomaly/Object/Bug/Anomaly_Object_Bug.h"
+
+#pragma region Activity
+
+void AAnomaly_Bug::ActivateAnomaly()
+{
+	Super::ActivateAnomaly();
+
+	switch (AnomalyName)
+	{
+	case EAnomalyName::Bug:
+		ActiveTrigger();
+		break;
+	}
+}
+
+void AAnomaly_Bug::StartAnomalyAction()
+{
+	SpawnBugs();
+}
+
+#pragma endregion
+
+#pragma region Bug
+
+void AAnomaly_Bug::SpawnBugs()
+{
+	for (const FVector& Target : BugLocations)
+	{
+		auto* SpawnedBug = GetWorld()->SpawnActor<AAnomaly_Object_Bug>(BugClass, Target, FRotator::ZeroRotator);
+		SpawnedBug->ActiveBug();
+	}
+}
+
+#pragma endregion
---
+++ Source/Endless_Hotel/Anomaly/EightExit/Bug/Anomaly_Bug.h
@@ -0,0 +1,38 @@
+﻿// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Anomaly/Base/Anomaly_Base_EightExit.h"
+#include <CoreMinimal.h>
+#include <Anomaly_Bug.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Bug : public AAnomaly_Base_EightExit
+{
+	GENERATED_BODY()
+
+#pragma region Activity
+
+public:
+	virtual void ActivateAnomaly() override;
+
+protected:
+	virtual void StartAnomalyAction() override;
+
+#pragma endregion
+
+#pragma region Bug
+
+protected:
+	void SpawnBugs();
+
+protected:
+	UPROPERTY(EditAnywhere, Category = "Bug")
+	TArray<FVector> BugLocations;
+
+	UPROPERTY(EditAnywhere, Category = "Bug")
+	TSubclassOf<class AAnomaly_Object_Bug> BugClass;
+
+#pragma endregion
+
+};
