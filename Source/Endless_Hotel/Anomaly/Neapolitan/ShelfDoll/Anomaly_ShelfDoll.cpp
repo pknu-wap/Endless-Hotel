c97c595e29285baa5f7a58170ff4129bb37755e9
ssiver6324 <ssiver6324@gmail.com>
2026-01-26 23:24:01 +0900

    Fix: 인형 코드 수정 중

--- Source/Endless_Hotel/Anomaly/Neapolitan/ShelfDoll/Anomaly_ShelfDoll.cpp
+++ Source/Endless_Hotel/Anomaly/Neapolitan/ShelfDoll/Anomaly_ShelfDoll.cpp
@@ -3,6 +3,9 @@
 
 #include "Anomaly/Neapolitan/ShelfDoll/Anomaly_ShelfDoll.h"
 #include "Anomaly/Object/ShelfDoll/Anomaly_Object_ShelfDoll.h"
+#include "Anomaly/Object/ShelfBook/Anomaly_Object_ShelfBook.h"
+#include "Anomaly/Object/Plant/Anomaly_Object_Plant.h"
+#include <Kismet/GameplayStatics.h>
 
 void AAnomaly_ShelfDoll::SetAnomalyActivate()
 {
@@ -11,24 +14,31 @@ void AAnomaly_ShelfDoll::SetAnomalyActivate()
 	switch (AnomalyName)
 	{
 	case EAnomalyName::Shelf_Doll:
-			AnomalyAction = ([](AAnomaly_Object_Base* Object)
+		AnomalyAction = ([](AAnomaly_Object_Base* Object)
+		{
+			if (auto* Doll = Cast<AAnomaly_Object_ShelfDoll>(Object))
 			{
-				AAnomaly_Object_ShelfDoll* ShelfDollObj = Cast<AAnomaly_Object_ShelfDoll>(Object);
-				if (!ShelfDollObj)
-				{
-					return;
-				}
+				Doll->ActivateDoll_Show();
+				Doll->SetInteraction();
+			}
 
-				ShelfDollObj->ActivateDoll_Show();
-				ShelfDollObj->ActivateShelfBook_Hide();
-				ShelfDollObj->ActivatePlant_Hide();
-				Cast<AAnomaly_Object_ShelfDoll>(Object)->SetInteraction();
-			});
-			break;
-
-		default:
-			break;
+			TArray<AActor*> Books;
+			UGameplayStatics::GetAllActorsOfClass(Object->GetWorld(), AAnomaly_Object_ShelfBook::StaticClass(), Books);
+			for (AActor* A : Books)
+			{
+				if (auto* Book = Cast<AAnomaly_Object_ShelfBook>(Object))
+					Book->StartBookOff();
+			}
+			
+			TArray<AActor*> Plants;
+			UGameplayStatics::GetAllActorsOfClass(Object->GetWorld(), AAnomaly_Object_Plant::StaticClass(), Plants);
+			for (AActor* A : Plants)
+			{
+				if (auto* Plant = Cast<AAnomaly_Object_Plant>(Object))
+					Plant->StartPlantOff();
+			}
+		});
+		ScheduleAnomaly();
+		break;
 	}
-
-	StartAnomalyAction();
 }
