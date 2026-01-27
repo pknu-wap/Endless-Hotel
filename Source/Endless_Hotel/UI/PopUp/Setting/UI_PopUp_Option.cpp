fed7b529ac02a6b7140d1b365fac2c2966c5d48c
이상혁 <imddongs@naver.com>
2026-01-27 16:25:33 +0900

    Refactor: 설정 UI 코드 리팩토링

--- Source/Endless_Hotel/UI/PopUp/Setting/UI_PopUp_Option.cpp
+++ Source/Endless_Hotel/UI/PopUp/Setting/UI_PopUp_Option.cpp
@@ -1,3 +1,35 @@
 ﻿// Copyright by 2025-2 WAP Game 2 team
 
 #include "UI/PopUp/Setting/UI_PopUp_Option.h"
+#include "UI/ComboBox/Setting/UI_ComboBox_Setting.h"
+
+#pragma region Base
+
+void UUI_PopUp_Option::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	ComboBox_Default = Cast<UUI_ComboBox_Setting>(GetWidgetFromName(TEXT("ComboBox")));
+
+	if (ComboBox_Default)
+	{
+		ComboBox_Default->ClearOptions();
+
+		for (auto Value : ComboBoxOptions)
+		{
+			ComboBox_Default->AddEnumOption<EOptionValue>(Value);
+		}
+	}
+}
+
+void UUI_PopUp_Option::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ComboBox_Default)
+	{
+		ComboBox_Default->BindEvents();
+	}
+}
+
+#pragma endregion
--- Source/Endless_Hotel/UI/PopUp/Setting/UI_PopUp_Option.h
+++ Source/Endless_Hotel/UI/PopUp/Setting/UI_PopUp_Option.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "UI/PopUp/UI_PopUp_Base.h"
+#include "Type/UI/Type_UI_Setting.h"
 #include <CoreMinimal.h>
 #include <UI_PopUp_Option.generated.h>
 
@@ -10,7 +11,15 @@ UCLASS(Meta = (DisableNativeTick))
 class ENDLESS_HOTEL_API UUI_PopUp_Option : public UUI_PopUp_Base
 {
 	GENERATED_BODY()
-	
+
+#pragma region Base
+
+protected:
+	virtual void NativeOnInitialized() override;
+	virtual void NativeConstruct() override;
+
+#pragma endregion
+
 #pragma region Input
 
 public:
@@ -18,10 +27,14 @@ public:
 
 #pragma endregion
 
-#pragma region Restore
+#pragma region ComboBox
 
 protected:
-	virtual void RestoreOptions() PURE_VIRTUAL(ThisClass::RestoreOptions, );
+	UPROPERTY()
+	TObjectPtr<class UUI_ComboBox_Setting> ComboBox_Default;
+
+	UPROPERTY(EditAnywhere, Category = "Setting|ComboBox")
+	TArray<EOptionValue> ComboBoxOptions;
 
 #pragma endregion
 
