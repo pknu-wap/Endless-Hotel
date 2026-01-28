af637115cb0aa8633055f9dcff09fb908574d9ec
bumyee11 <gd132229@gmail.com>
2026-01-29 00:17:39 +0900

    Feat: 양자택일 일반 맵에 배치되는 키
    2개 중 하나의 값을 게임인스턴스에 저장

---
+++ Source/Endless_Hotel/Anomaly/EightExit/Choose/Anomaly_Choose.h
@@ -0,0 +1,27 @@
+// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Anomaly/Base/Anomaly_Base_EightExit.h"
+#include <CoreMinimal.h>
+#include <Anomaly_Choose.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Choose : public AAnomaly_Base_EightExit
+{
+	GENERATED_BODY()
+
+#pragma region Base
+
+public:
+	AAnomaly_Choose(const FObjectInitializer& ObjectInitializer);
+
+#pragma endregion
+
+#pragma region Activity
+
+public:
+	virtual void SetAnomalyActivate() override;
+
+#pragma endregion
+};
