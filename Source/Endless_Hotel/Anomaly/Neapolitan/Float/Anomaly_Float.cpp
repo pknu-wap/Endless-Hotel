40dae9be00db0a9bb281fbd12d3f6c0af865fe98
bumyee11 <gd132229@gmail.com>
2026-01-28 22:18:19 +0900

    Feat: 중력 이상현상, 복구 컴포넌트 추가

--- Source/Endless_Hotel/Anomaly/Neapolitan/Float/Anomaly_Float.cpp
+++ Source/Endless_Hotel/Anomaly/Neapolitan/Float/Anomaly_Float.cpp
@@ -3,6 +3,9 @@
 
 #include "Anomaly/Neapolitan/Float/Anomaly_Float.h"
 #include "Anomaly/Object/Float/Anomaly_Object_Float.h"
+#include "Component/Anomaly_Component_Restore.h"
+#include <EngineUtils.h>
+#include "Component/Anomaly_Float/Anomaly_Component_Float.h"
 
 #pragma region Activity
 
@@ -13,10 +16,28 @@ void AAnomaly_Float::SetAnomalyActivate()
 	switch (AnomalyName)
 	{
 	case EAnomalyName::Float:
-		AnomalyAction = ([](AAnomaly_Object_Base* Float)
+		AnomalyAction = ([this](AAnomaly_Object_Base* Unused)
 			{
-				Cast<AAnomaly_Object_Float>(Float)->StartFloating();
-				Cast<AAnomaly_Object_Float>(Float)->SetInteraction();
+                for (TActorIterator<AActor> It(GetWorld()); It; ++It)
+                {
+                    AActor* Target = *It;
+                    auto* FloatComp = Target->FindComponentByClass<UAnomaly_Component_Float>();
+                    auto* RestoreComp = Target->FindComponentByClass<UAnomaly_Component_Restore>();
+
+                    if (FloatComp && RestoreComp)
+                    {
+                        FloatComp->StartFloating();
+
+                        if (auto* BaseObj = Cast<AAnomaly_Object_Base>(Target))
+                        {
+                            BaseObj->InteractAction = [FloatComp, RestoreComp]()
+                                {
+                                    FloatComp->StopFloating();
+                                    RestoreComp->StartRestoring(2.5f);
+                                };
+                        }
+                    }
+                }
 			});
 		ScheduleAnomaly(10);
 		break;
---
+++ Source/Endless_Hotel/Component/Anomaly_Component_Restore.h
@@ -0,0 +1,42 @@
+// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Component/EHComponent.h"
+#include <CoreMinimal.h>
+#include <Anomaly_Component_Restore.generated.h>
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ENDLESS_HOTEL_API UAnomaly_Component_Restore : public UEHComponent
+{
+    GENERATED_BODY()
+
+#pragma region Base
+
+public:
+    UAnomaly_Component_Restore(const FObjectInitializer& ObjectInitializer);
+
+protected:
+    virtual void BeginPlay() override;
+
+#pragma endregion 
+
+#pragma region Restore
+
+public:
+    void SaveOriginalTransform();
+
+    void StartRestoring(float Duration = 2.5f);
+
+private:
+    void RestoreTick();
+
+    FTransform OriginalTransform;
+    FTimerHandle RestoreHandle;
+
+    float RestoreDuration = 2.5f;
+    float RestoreCurrentTime = 0.f;
+
+#pragma endregion
+
+};
