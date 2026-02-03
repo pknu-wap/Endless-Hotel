2e6a9ce235be0b4fa0c9fc8b03cbbd104f17b1c4
kwkim526 <polops2331@gmail.com>
2026-02-03 11:37:49 +0900

    Feat: 플레이어 사망 애니메이션 추가
    -플레이어 사망 애니메이션 추가
    -미로 맵 AI 배치
    -플레이어 사망 중복 처리 방지 변수 추가
    -시작 위치 변경

--- Source/Endless_Hotel/Player/Character/EHPlayer.cpp
+++ Source/Endless_Hotel/Player/Character/EHPlayer.cpp
@@ -32,6 +32,7 @@ void AEHPlayer::BeginPlay()
 
 void AEHPlayer::DiePlayer(const EDeathReason& DeathReason)
 {
+	bIsDead = true;
 	UAnimMontage* DeathAnim = DeathAnims[DeathReason];
 
 	AEHPlayerController* PC = Cast<AEHPlayerController>(GetController());
