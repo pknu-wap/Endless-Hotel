f88df6df7c53693be7cb3ad579994a68cdf1f5de
ssiver6324 <ssiver6324@gmail.com>
2026-02-19 09:01:15 +0900

    Fix: 책 식물 합치기

---
+++ Source/Endless_Hotel/Anomaly/EightExit/Shelf/Anomaly_Shelf.h
@@ -0,0 +1,20 @@
+// Copyright by 2025-2 WAP Game 2 team
+
+#pragma once
+
+#include "Anomaly/Base/Anomaly_Base_EightExit.h"
+#include <CoreMinimal.h>
+#include <Anomaly_Shelf.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API AAnomaly_Shelf : public AAnomaly_Base_EightExit
+{
+	GENERATED_BODY()
+	
+#pragma region Activity
+
+public:
+	virtual void SetAnomalyActivate() override;
+
+#pragma endregion
+};
