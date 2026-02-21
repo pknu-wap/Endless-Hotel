af769a0335c186e9642995161cda29accb79886b
kwkim526 <polops2331@gmail.com>
2026-02-21 18:53:33 +0900

    Fix&Refactor: 엘리베이터 수정
    - 엘리베이터 코드 리팩토링
    - 엘리베이터 애니메이션 맞추기
    - 엘리베이터 버튼 상호작용 불가 버그 수정
    - 엘리베이터 중앙 안 맞는 부분 수정

--- Source/Endless_Hotel/Actor/Elevator/Elevator.h
+++ Source/Endless_Hotel/Actor/Elevator/Elevator.h
@@ -1,244 +1,223 @@
-﻿// Elevator.h
-#pragma once
+﻿#pragma once
 
 #include "Actor/EHActor.h"
 #include <CoreMinimal.h>
-#include <Delegates/DelegateCombinations.h>
 #include <Elevator.generated.h>
 
-#pragma region ForwardDeclarations
-
-class UPointLightComponent;
-class UStaticMeshComponent;
-class UBoxComponent;
-class USceneComponent;
-class UTimelineComponent;
-class UCurveFloat;
-class AElevator_Button;
-
-#pragma endregion
-
-#pragma region Declare
+UENUM(BlueprintType)
+enum class EElevatorState : uint8
+{
+    Idle,
+    DoorMoving,
+    Moving,
+    Finished
+};
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FElevatorDelegate, bool, bIsStart);
-
-#pragma endregion
 
 UCLASS()
 class ENDLESS_HOTEL_API AElevator : public AEHActor
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 #pragma region Base
 
 public:
-	AElevator(const FObjectInitializer& ObjectInitializer);
+    AElevator(const FObjectInitializer& ObjectInitializer);
+    static FElevatorDelegate ElevatorDelegate;
 
 protected:
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
+
+    UFUNCTION()
+    void OnInsideBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnInsideEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 protected:
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Elevator")
-	TObjectPtr<USceneComponent> ElevatorSceneRoot;
+    UPROPERTY(VisibleAnywhere, Category = "Frame")
+    TObjectPtr<UStaticMeshComponent> Exterior_Structure;
 
-#pragma endregion
+    UPROPERTY(VisibleAnywhere, Category = "Frame")
+    TObjectPtr<UStaticMeshComponent> Entrance;
 
-#pragma region Frames
+    UPROPERTY(VisibleAnywhere, Category = "Frame")
+    TObjectPtr<UStaticMeshComponent> Car;
 
-protected:
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Frame")
-	TObjectPtr<UStaticMeshComponent> Exterior_Structure;
+    UPROPERTY(VisibleAnywhere, Category = "Doors")
+    TObjectPtr<UStaticMeshComponent> LeftDoor;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Frame")
-	TObjectPtr<UStaticMeshComponent> Entrance;
+    UPROPERTY(VisibleAnywhere, Category = "Doors")
+    TObjectPtr<UStaticMeshComponent> RightDoor;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Frame")
-	TObjectPtr<UStaticMeshComponent> Car;
+    UPROPERTY(VisibleAnywhere, Category = "Doors")
+    TObjectPtr<UStaticMeshComponent> RightGlass;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Doors")
-	TObjectPtr<UStaticMeshComponent> LeftDoor;
+    UPROPERTY(VisibleAnywhere, Category = "Doors")
+    TObjectPtr<UStaticMeshComponent> LeftGlass;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Doors")
-	TObjectPtr<UStaticMeshComponent> RightDoor;
+    UPROPERTY(EditAnywhere, Category = "Floor")
+    TObjectPtr<UStaticMeshComponent> Floor;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Doors")
-	TObjectPtr<UStaticMeshComponent> RightGlass;
+    UPROPERTY(VisibleAnywhere, Category = "Light")
+    TObjectPtr<class UPointLightComponent> ElevatorLight;
 
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Doors")
-	TObjectPtr<UStaticMeshComponent> LeftGlass;
+    UPROPERTY(VisibleAnywhere, Category = "Trigger")
+    TObjectPtr<class UBoxComponent> InsideTrigger;
 
-#pragma endregion
+    UPROPERTY(VisibleAnywhere, Category = "Trigger")
+    TObjectPtr<UStaticMeshComponent> TriggerBlockBox;
 
-#pragma region Light
-
-protected:
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Light")
-	TObjectPtr<UPointLightComponent> ElevatorLight;
-
-	const float LightOnIntensity = 100000.f;
-	const float LightOffIntensity = 5000.f;
-
-#pragma endregion
-
-#pragma region Sound
-
-protected:
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<class UAudioComponent> Elevator_AC;
-
-	UPROPERTY(EditAnywhere)
-	TObjectPtr<class UAudioComponent> Door_AC;
-
-	UPROPERTY(EditAnywhere, Category = "Sound")
-	TObjectPtr<class USoundWave> Sound_DoorMove;
-
-	UPROPERTY(EditAnywhere, Category = "Sound")
-	TObjectPtr<class USoundWave> Sound_ElevatorMove;
-
-#pragma endregion
-
-#pragma region Trigger
-
-protected:
-	// InsideTrigger
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Trigger")
-	TObjectPtr<UBoxComponent> InsideTrigger;
-
-	UPROPERTY(EditAnywhere, Category = "Trigger")
-	TObjectPtr<UStaticMeshComponent> TriggerBlockBox;
-
-protected:
-	UFUNCTION()
-	void OnInsideBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-
-	UFUNCTION()
-	void OnInsideEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
-		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
-
-#pragma endregion
-
-#pragma region DoorMovement
-
-public:
-	void MoveDoors(bool isOpening);
-
-	void TryCloseDoorAfterDelay();
-
-	UFUNCTION()
-	void OnDoorTimelineUpdate(float Alpha);
-
-	UFUNCTION()
-	void OnDoorTimelineFinished();
-
-protected:
-	UFUNCTION()
-	void OpenDoorAfterMove();
-
-public:
-	bool bIsDoorMoving;
-	bool bIsDoorOpened;
-
-protected:
-	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Movement|Door")
-	TObjectPtr<UTimelineComponent> DoorTimeline;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement|Door")
-	TObjectPtr<UCurveFloat> DoorCurve;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Door", meta = (ClampMin = "0.0", UIMin = "0.0"))
-	float DoorDuration = 1.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Door")
-	float DoorGap = 40.0f;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement|Door")
-	bool bSlideOnX = true;
+    UPROPERTY(VisibleAnywhere, Category = "UI")
+    TObjectPtr<UStaticMeshComponent> StickerPannel;
 
 private:
-	FVector LeftDoorClosed, RightDoorClosed;
-	FVector LeftDoorOpenPos, RightDoorOpenPos;
-
-	FTimerHandle DoorCloseTimerHandle;
-
-	bool bIsPlayerInside;
+    EElevatorState CurrentState = EElevatorState::Idle;
+    bool bIsPlayerInside = false;
 
 #pragma endregion
 
-#pragma region ElevatorMovement
-
-public:
-	static FElevatorDelegate ElevatorDelegate;
-
-	UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
-	bool bIsMapStartElevator;
+#pragma region LightSettings
 
 protected:
-	UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
-	FVector StartPos;
+    UPROPERTY(EditAnywhere, Category = "Setting|LightSettings")
+    float LightOnIntensity = 100000.f;
 
-	UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
-	FVector MapPos;
+    UPROPERTY(EditAnywhere, Category = "Setting|LightSettings")
+    float LightOffIntensity = 5000.f;
 
-	UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
-	FVector EndPos;
+#pragma endregion
 
-	UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
-	float ElevatorMoveDuration = 3.0f;
-
-public:
-	void CallElevator();
+#pragma region Audio
 
 protected:
-	void ElevatorMove(FVector Start, FVector End, bool bIsStart);
+    UPROPERTY(VisibleAnywhere, Category = "Audio")
+    TObjectPtr<class UAudioComponent> Elevator_AC;
+
+    UPROPERTY(VisibleAnywhere, Category = "Audio")
+    TObjectPtr<class UAudioComponent> Door_AC;
+
+    UPROPERTY(EditAnywhere, Category = "Audio")
+    TObjectPtr<class USoundWave> Sound_DoorMove;
+
+    UPROPERTY(EditAnywhere, Category = "Audio")
+    TObjectPtr<class USoundWave> Sound_ElevatorMove;
+
+#pragma endregion
+
+#pragma region MovementSettings
+
+public:
+    void MoveDoors(bool bIsOpening);
+
+    UFUNCTION()
+    void OnDoorTimelineUpdate(float Alpha);
+
+    UFUNCTION()
+    void OnDoorTimelineFinished();
+
+protected:
+    void ElevatorMove(FVector Start, FVector End, bool bIsStart);
+    void ExecuteElevatorRide();
+
+    UFUNCTION()
+    void OpenDoorAfterMove();
+
+    UFUNCTION()
+    void BroadcastElevatorFinished();
+
+protected:
+    UPROPERTY(EditAnywhere, Category = "Movement")
+    TObjectPtr<class UTimelineComponent> DoorTimeline;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Door")
+    TObjectPtr<UCurveFloat> DoorCurve;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Door")
+    float DoorDuration = 1.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Door|Closed")
+    FVector LeftDoorClosed;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Door|Closed")
+    FVector RightDoorClosed;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Door|Opened")
+    FVector LeftDoorOpenPos;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Door|Opened")
+    FVector RightDoorOpenPos;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
+    FVector StartPos;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
+    FVector MapPos;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
+    FVector EndPos;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
+    float ElevatorMoveDuration = 3.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
+    bool bIsMapStartElevator;
 
 private:
-	FTimerHandle MoveHandle;
+    FTimerHandle MoveHandle;
+    FTimerHandle BoardingCloseTimerHandle;
+    bool bShouldMoveAfterClose = false;
+    bool bIsDoorOpened = false;
+    bool bIsDoorMoving = false;
 
 #pragma endregion
 
 #pragma region Player
 
 protected:
-	UPROPERTY(EditAnywhere)
-	FRotator RotateAngle;
+    void SetPlayerInputEnabled(bool bEnable);
 
-	FTimerHandle RotateHandle;
+    void RotatePlayer();
 
-	UPROPERTY(EditAnywhere, Category = "Position")
-	FVector2D PlayerLocationInElevator;
+    void SmoothRotate(FRotator TargetRotation);
+
+    void TakePlayer();
 
 protected:
-	void RotatePlayer();
-	void SmoothRotate(FRotator OriginRoatation);
-	void TakePlayer();
+    UPROPERTY(EditAnywhere, Category = "Player")
+    FRotator RotateAngle;
 
-private:
-	void SetPlayerInputEnabled(bool bEnable);
+    UPROPERTY(EditAnywhere, Category = "Player")
+    FVector2D PlayerLocationInElevator;
+
+    FTimerHandle RotateHandle;
+
+#pragma endregion
+
+#pragma region Button
+
+public:
+    UFUNCTION()
+    void OnButtonClicked();
+
+protected:
+    UPROPERTY(EditAnywhere, Category = "Elevator|Button")
+    TObjectPtr<class AElevator_Button> AttachedButton;
 
 #pragma endregion
 
 #pragma region Subsystem
 
 protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Type")
-	bool bIsNormalElevator = true;
-
-private:
-	bool bChoiceSentThisRide = false;
-
-private:
-	void NotifySubsystemElevatorChoice();
-
-#pragma endregion
-
-#pragma region UI
+    void NotifySubsystemElevatorChoice();
 
 protected:
-	UPROPERTY(EditAnywhere, Category = "UI")
-	TObjectPtr<class UStaticMeshComponent> StickerPannel;
+    UPROPERTY(EditAnywhere, Category = "Type")
+    bool bIsNormalElevator = true;
+
+private:
+    bool bChoiceSentThisRide = false;
 
 #pragma endregion
-
-
 };
