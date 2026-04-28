feeb06b42b215b4a17f7d67b8c1e92e3f11ecb42
ssiver6324 <ssiver6324@gmail.com>
2026-04-29 01:19:12 +0900

    Feat: 라디오 이상현상 제작
    상호작용 설정은 해두었으나 작동 안됨

---
+++ Source/Endless_Hotel/Anomaly/Object/Neapolitan/Radio/Anomaly_Object_Radio.cpp
@@ -0,0 +1,73 @@
+// Copyright by 2026-1 WAP Game 2 team
+
+#include "Anomaly/Object/Neapolitan/Radio/Anomaly_Object_Radio.h"
+#include <Components/StaticMeshComponent.h>
+#include <Components/TimelineComponent.h>
+#include <Components/AudioComponent.h>
+#include <Kismet/GameplayStatics.h>
+
+#pragma region Base
+
+AAnomaly_Object_Radio::AAnomaly_Object_Radio(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+	SM_Radio_Pointer = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("SM_Radio_Pointer"));
+	SM_Radio_Pointer->SetupAttachment(Object);
+
+	Timeline_PointerSpin = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline_PointerSpin"));
+
+	AC = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AC->SetupAttachment(RootComponent);
+}
+
+void AAnomaly_Object_Radio::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OriginalPointerRotation = SM_Radio_Pointer->GetRelativeRotation();
+
+	FOnTimelineFloat Update_PointerSpin;
+	Update_PointerSpin.BindUFunction(this, "UpdatePointerSpin");
+	Timeline_PointerSpin->AddInterpFloat(CV_PointerSpin, Update_PointerSpin);
+
+	FOnTimelineEvent Finish_PointerSpin;
+	Finish_PointerSpin.BindUFunction(this, "FinishMove");
+	Timeline_PointerSpin->SetTimelineFinishedFunc(Finish_PointerSpin);
+}
+
+#pragma region Radio
+
+void AAnomaly_Object_Radio::PointerMoving()
+{
+	bIsPlaying = true;
+	
+	AC->Sound = Sound_Radio;
+	AC->Play();
+
+	Timeline_PointerSpin->PlayFromStart();
+}
+
+#pragma endregion
+
+#pragma region RotateMove
+
+void AAnomaly_Object_Radio::FinishMove()
+{
+	bIsPlaying = false;
+
+	AC->Stop();
+
+	Timeline_PointerSpin->Stop();
+}
+
+#pragma endregion
+
+#pragma region PointerTimeline
+
+void AAnomaly_Object_Radio::UpdatePointerSpin(float Value)
+{
+	FRotator Target = OriginalPointerRotation;
+	Target.Roll = OriginalPointerRotation.Roll + Value;
+	SM_Radio_Pointer->SetRelativeRotation(Target);
+}
+#pragma endregion
--- Source/Endless_Hotel/Type/Anomaly/Type_AnomalyName.h
+++ Source/Endless_Hotel/Type/Anomaly/Type_AnomalyName.h
@@ -44,5 +44,6 @@ enum class EAnomalyName : uint8
 	Painting_Blood		= 96	UMETA(DisplayName = "Painting_Blood"),
 	Maze_Monster		= 97	UMETA(DisplayName = "Maze_Monster"),
 	Doll				= 98	UMETA(DisplayName = "Doll"),
-	Painting_Eye		= 99	UMETA(DisplayName = "Painting_Eye")
+	Painting_Eye		= 99	UMETA(DisplayName = "Painting_Eye"),
+	Radio				= 100	UMETA(DisplayName = "Radio")
 };
