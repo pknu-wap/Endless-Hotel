6e427d98d885bc754ae9b8e843e08adab28c2101
kwkim526 <polops2331@gmail.com>
2026-05-11 02:03:48 +0900

    Fix: 상호작용 버그 수정
    - 변수명 오타 버그 수정

--- Source/Endless_Hotel/Anomaly/Base/Anomaly_Event.h
+++ Source/Endless_Hotel/Anomaly/Base/Anomaly_Event.h
@@ -131,12 +131,12 @@ protected:
 	template<typename ObjectType>
 	void SetupAnomalyAction(void (ObjectType::* SelectedFunc)(), TArray<EInteractType> Interactions = { EInteractType::None }, bool bIsOrdered = false)
 	{
-		AnomalyActions.Add([SelectedFunc, Interactions](AAnomaly_Object_Base* Obj)
+		AnomalyActions.Add([SelectedFunc, Interactions, bIsOrdered](AAnomaly_Object_Base* Obj)
 			{
 				if (ObjectType* TargetObj = Cast<ObjectType>(Obj))
 				{
 					TargetObj->CorrectInteractTypes = Interactions;
-					TargetObj->bHasOrderedInteractTypes = bIsOrdered;
+					TargetObj->bIsOrderedInteractTypes = bIsOrdered;
 					(TargetObj->*SelectedFunc)();
 				}
 			});
