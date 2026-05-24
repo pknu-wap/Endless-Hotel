4b38b3e00d625c7b61ad70440511b865fbbdccb0
kwkim526 <polops2331@gmail.com>
2026-05-25 07:32:22 +0900

    Fix: 엘리베이터 낙사 수정
    - 엘리베이터 추락하면 낙사하도록 수정

--- Source/Endless_Hotel/Anomaly/EightExit/Elevator/Anomaly_Elevator.h
+++ Source/Endless_Hotel/Anomaly/EightExit/Elevator/Anomaly_Elevator.h
@@ -24,6 +24,9 @@ protected:
 	UPROPERTY(EditAnywhere, Category = "Movement|Elevator")
 	FName TargetAnomalyElevatorID;
 
+	virtual void OnTriggerBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+
 private:
 	TWeakObjectPtr<class AElevator> TargetElevator;
 
