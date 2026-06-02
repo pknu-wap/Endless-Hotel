81ce59b051b6d4c4c06006679dfd4806826b716a
kwkim526 <polops2331@gmail.com>
2026-06-03 00:24:23 +0900

    Fix: Twin 수정
    - Twin 나오도록 수정

--- Source/Endless_Hotel/Anomaly/Object/Neapolitan/Twin/Anomaly_Object_Twin.cpp
+++ Source/Endless_Hotel/Anomaly/Object/Neapolitan/Twin/Anomaly_Object_Twin.cpp
@@ -1,24 +1,13 @@
 ﻿// Copyright by 2025-2 WAP Game 2 team
 
-
 #include "Anomaly/Object/Neapolitan/Twin/Anomaly_Object_Twin.h"
 #include <Components/BoxComponent.h>
 
-#pragma region Base
-
-//AAnomaly_Object_Twin::AAnomaly_Object_Twin(const FObjectInitializer& ObjectInitializer)
-//	:Super(ObjectInitializer)
-//{
-//	//이거 어떻게 설정할 지를 모르겠는데
-//}
-//
-//void AAnomaly_Object_Twin::BeginPlay()
-//{
-//	Super::BeginPlay();
-//}
-
-#pragma endregion 
-
 #pragma region Twin
+
+void AAnomaly_Object_Twin::StartTwin()
+{
+	SetActorTransform(TwinTransform);
+}
 
 #pragma endregion
