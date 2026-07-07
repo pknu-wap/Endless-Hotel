542b0fe13175b12a2e40aaa630773053d00c5747
kwkim526 <polops2331@gmail.com>
2026-07-07 10:25:53 +0900

    Fix: 엘리베이터 카메라 오류 수정
    - 엘리베이터 카메라 오류 임시 수정 (플레이어 코드 개선되면 사라질 문제인 것 같기도 함)

---
+++ Source/Endless_Hotel/Character/AI/CeilingGhost/CeilingGhostController.h
@@ -0,0 +1,14 @@
+﻿// Copyright by 2026-1 WAP Game 2 team
+
+#pragma once
+
+#include "Character/AI/BaseAIController.h"
+#include <CoreMinimal.h>
+#include <CeilingGhostController.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API ACeilingGhostController : public ABaseAIController
+{
+	GENERATED_BODY()
+	
+};
