e665104986ec279c8d18a1b343f7c4a49e481564
kwkim526 <polops2331@gmail.com>
2026-08-02 17:49:15 +0900

    Fix: 데이터 로직 변경
    - DataController 제거 및 AssetManager로 이동
    - 디버깅용 함수들 추가

--- Source/Endless_Hotel/Asset/Manager/EHAssetManager.h
+++ Source/Endless_Hotel/Asset/Manager/EHAssetManager.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "Type/Anomaly/Type_AnomalyEntry.h"
+#include "Type/Anomaly/Type_AnomalyID.h"
+#include "Type/Anomaly/Type_AnomalyRule.h"
 #include <CoreMinimal.h>
 #include <Engine/AssetManager.h>
 #include <EHAssetManager.generated.h>
@@ -19,7 +21,7 @@ public:
 
 #pragma endregion
 
-#pragma region Anomaly
+#pragma region AnomalyData|Query
 
 public:
 	FAnomalyEntry GetAnomalyData(uint8 Index);
@@ -31,9 +33,69 @@ public:
 private:
 	void LoadAnomalyData();
 
+#pragma endregion
+
+#pragma region Entry|Setup
+
+public:
+	void InitAnomalyEntries();
+	void RebuildActAnomalyFromOrigin();
+	void RemoveNoRuleAnomaly(const TArray<EAnomalyRule>& ActiveRules);
+	void ShuffleActAnomaly();
+
+#pragma endregion
+
+#pragma region Anomaly|ClearState
+
+public:
+	void ResetClearedAnomaly();
+	void RemoveClearedAnomaly();
+	void MarkAnomalyCleared(EAnomalyID ID) { ClearedAnomalySet.Add(ID); }
+
+	int32 GetClearedAnomalyCount() const { return ClearedAnomalySet.Num(); }
+	bool IsClearedAnomalySetEmpty() const { return ClearedAnomalySet.IsEmpty(); }
+	TArray<EAnomalyID> GetClearedAnomalySet() const { return ClearedAnomalySet.Array(); }
+
+#pragma endregion
+
+#pragma region Anomaly|Getter
+
+public:
+	uint8 GetRemainingAnomalyCounts() const;
+	TArray<TSubclassOf<AAnomaly_Object_Base>> GetObjectByID(EAnomalyID AnomalyID);
+	const FAnomalyEntry& GetNormalAnomalyData() const { return NormalAnomalyData; }
+	const FAnomalyEntry& GetActAnomalyByIndex(uint8 Index) const { return ActAnomaly.IsValidIndex(Index) ? ActAnomaly[Index] : NormalAnomalyData; }
+	const TArray<FAnomalyEntry>& GetOriginAnomaly() const { return OriginAnomaly; }
+	bool IsValidActAnomalyIndex(uint8 Index) const { return ActAnomaly.IsValidIndex(Index); }
+	bool CanSpawnAnomaly(const FAnomalyEntry& AnomalyEntry, const TArray<EAnomalyRule>& ActiveRules) const;
+
+#pragma endregion
+
+#pragma region Anomaly|Data
+
 private:
 	UPROPERTY(Transient)
 	TArray<FAnomalyEntry> Data_Anomalies;
+
+	UPROPERTY(Transient)
+	TArray<FAnomalyEntry> OriginAnomaly;
+
+	UPROPERTY(Transient)
+	TArray<FAnomalyEntry> ActAnomaly;
+
+	UPROPERTY(Transient)
+	FAnomalyEntry NormalAnomalyData;
+
+	TSet<EAnomalyID> ClearedAnomalySet;
+
+#pragma endregion
+
+#pragma region ForDebug
+
+public:
+	FString GetActAnomalyListAsString() const;
+	void AddToSpawnList(EAnomalyID AnomalyID);
+	void RemoveFromSpawnList(EAnomalyID AnomalyID);
 
 #pragma endregion
 
