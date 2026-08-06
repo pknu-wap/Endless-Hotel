6f615343f7825c0b6b2e3bd6acf149ccd77b8617
심서은 <ssiver6324@gmail.com>
2026-08-07 02:33:09 +0900

    Fix: Door작동 시키기

--- Source/Endless_Hotel/Anomaly/Event/EightExit/Door/Anomaly_Door.cpp
+++ Source/Endless_Hotel/Anomaly/Event/EightExit/Door/Anomaly_Door.cpp
@@ -2,6 +2,8 @@
 
 #include "Anomaly/Event/EightExit/Door/Anomaly_Door.h"
 #include "Anomaly/Object/EightExit/Door/Anomaly_Object_Door.h"
+#include "Player/Character/EHPlayer.h"
+#include <TimerManager.h>
 #include <Components/BoxComponent.h>
 
 #pragma region Base
@@ -10,10 +12,6 @@ AAnomaly_Door::AAnomaly_Door(const FObjectInitializer& ObjectInitializer)
 	:Super(ObjectInitializer)
 {
 	TriggerBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-
-	TriggerBox_Close = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox_Close"));
-	TriggerBox_Close->SetupAttachment(RootComponent);
-	TriggerBox_Close->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 #pragma endregion
@@ -29,6 +27,89 @@ void AAnomaly_Door::SetAnomalyState()
 	case EAnomalyID::Door_Shake:
 		SetupAnomalyAction<AAnomaly_Object_Door>(&AAnomaly_Object_Door::StartShaking);
 		ActiveTrigger();
+		break;
+
+	case EAnomalyID::Door_Close:
+		CurrentDoorState = EDoorSequenceState::OpenDoor;
+		TriggerBox->SetWorldTransform(OpenDoorTriggerTrans);
+		ActiveTrigger();
+		break;
+	}
+}
+
+#pragma endregion
+
+#pragma region Trigger
+
+void AAnomaly_Door::OnTriggerBox(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	AEHPlayer* Player = Cast<AEHPlayer>(OtherActor);
+	TriggerBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	AdvanceDoorState();
+}
+
+#pragma endregion
+
+#pragma region DoorSequence
+
+void AAnomaly_Door::AdvanceDoorState()
+{
+	AAnomaly_Object_Door* Door = nullptr;
+
+	for (const auto& Target : TargetAnomalyObjects)
+	{
+		Door = Cast<AAnomaly_Object_Door>(Target);
+	}
+
+	switch (CurrentDoorState)
+	{
+	case EDoorSequenceState::OpenDoor:
+	{
+		Door->OpenDoor();
+
+		TriggerBox->SetWorldTransform(OpenHandTriggerTrans);
+
+		constexpr float Delay = 0.1f;
+		FTimerHandle EnableHandle;
+
+		GetWorld()->GetTimerManager().SetTimer(EnableHandle, FTimerDelegate::CreateWeakLambda(this,[this]()
+		{
+			TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		}),	Delay, false);
+
+		CurrentDoorState = EDoorSequenceState::OpenHand;
+		break;
+	}
+
+	case EDoorSequenceState::OpenHand:
+	{
+		Door->PlayHandOpen();
+
+		TriggerBox->SetWorldTransform(CloseDoorTriggerTrans);
+
+		constexpr float Delay = 0.1f;
+		FTimerHandle EnableHandle;
+
+		GetWorld()->GetTimerManager().SetTimer(EnableHandle, FTimerDelegate::CreateWeakLambda(this, [this]()
+		{
+			TriggerBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+		}),	Delay, false);
+
+		CurrentDoorState = EDoorSequenceState::CloseHandAndDoor;
+		break;
+	}
+
+	case EDoorSequenceState::CloseHandAndDoor:
+	{
+		Door->PlayHandClose();
+		Door->CloseDoor();
+
+		CurrentDoorState = EDoorSequenceState::Finished;
+		break;
+	}
+
+	case EDoorSequenceState::Finished:
+	default:
 		break;
 	}
 }
--- Source/Endless_Hotel/Anomaly/Object/EightExit/Door/Anomaly_Object_Door.cpp
+++ Source/Endless_Hotel/Anomaly/Object/EightExit/Door/Anomaly_Object_Door.cpp
@@ -6,6 +6,10 @@
 #include "Component/Interact/InteractComponent.h"
 #include "GameSystem/SubSystem/GameSystem.h"
 #include "GameSystem/SaveGame/SaveManager.h"
+#include <Animation/SkeletalMeshActor.h>
+#include <Engine/SkeletalMesh.h>
+#include <Components/SkeletalMeshComponent.h>
+#include <Animation/AnimationAsset.h>
 #include <Components/StaticMeshComponent.h>
 #include <Components/TimelineComponent.h>
 #include <Components/AudioComponent.h>
@@ -44,6 +48,14 @@ void AAnomaly_Object_Door::Reset()
 	Super::Reset();
 	Component_Interact->DeactiveInteract();
 	SetLight(false);
+
+	if (IsValid(SpawnedGhostHandActor))
+	{
+		SpawnedGhostHandActor->Destroy();
+	}
+
+	SpawnedGhostHandActor = nullptr;
+	SKM_GhostHand = nullptr;
 }
 
 void AAnomaly_Object_Door::BeginPlay()
@@ -353,6 +365,65 @@ void AAnomaly_Object_Door::OnPushMoveCompleted()
 			EHPC->OnPushDoorCompleted();
 			EHPC->SetIgnoreLookInput(false);
 		}), 1.0f, false);
+}
+
+#pragma endregion
+#pragma region GhostHand
+
+void AAnomaly_Object_Door::SpawnGhostHand()
+{
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	const FTransform SocketTransform = Object->GetSocketTransform(GhostHandSocketName, ERelativeTransformSpace::RTS_World);
+
+	SpawnedGhostHandActor = GetWorld()->SpawnActor<ASkeletalMeshActor>(ASkeletalMeshActor::StaticClass(), SocketTransform, SpawnParams); 
+	
+	SKM_GhostHand = SpawnedGhostHandActor->GetSkeletalMeshComponent();
+
+	SKM_GhostHand->SetSkeletalMeshAsset(GhostHandMesh);
+
+	SKM_GhostHand->SetSimulatePhysics(false);
+
+	SKM_GhostHand->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	SpawnedGhostHandActor->AttachToComponent(Object, FAttachmentTransformRules::SnapToTargetNotIncludingScale, GhostHandSocketName);
+}
+
+void AAnomaly_Object_Door::PlayHandOpen()
+{
+	SpawnGhostHand();
+
+	SKM_GhostHand->SetHiddenInGame(false);
+
+	SKM_GhostHand->SetAnimationMode(EAnimationMode::AnimationSingleNode);
+
+	SKM_GhostHand->SetAnimation(Anim_HandOpen);
+	SKM_GhostHand->SetPosition(0.f, false);
+	SKM_GhostHand->Play(false);
+}
+
+void AAnomaly_Object_Door::PlayHandClose()
+{
+	SKM_GhostHand->SetAnimationMode(EAnimationMode::AnimationSingleNode);
+
+	SKM_GhostHand->SetAnimation(Anim_HandClose);
+	SKM_GhostHand->SetPosition(0.f, false);
+	SKM_GhostHand->Play(false);
+
+	const float AnimationLength = Anim_HandClose->GetPlayLength();
+
+	if (AnimationLength > 0.f)
+	{
+		SpawnedGhostHandActor->SetLifeSpan(AnimationLength);
+	}
+	else
+	{
+		SpawnedGhostHandActor->Destroy();
+		SpawnedGhostHandActor = nullptr;
+		SKM_GhostHand = nullptr;
+	}
 }
 
 #pragma endregion
