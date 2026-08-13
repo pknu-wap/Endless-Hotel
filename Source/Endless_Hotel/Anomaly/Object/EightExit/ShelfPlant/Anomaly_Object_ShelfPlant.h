2d28f2355a65837d2dbd6c45b9a784f1fd083dab
이상혁 <imddongs@naver.com>
2026-08-14 03:04:09 +0900

    Fix: Plant Change 버그 수정

--- Source/Endless_Hotel/Anomaly/Object/EightExit/ShelfPlant/Anomaly_Object_ShelfPlant.h
+++ Source/Endless_Hotel/Anomaly/Object/EightExit/ShelfPlant/Anomaly_Object_ShelfPlant.h
@@ -1,4 +1,4 @@
-// Copyright by 2025-2 WAP Game 2 team
+﻿// Copyright by 2025-2 WAP Game 2 team
 
 #pragma once
 
@@ -15,6 +15,13 @@ class ENDLESS_HOTEL_API AAnomaly_Object_ShelfPlant : public AAnomaly_Object_Eigh
 
 public:
 	AAnomaly_Object_ShelfPlant(const FObjectInitializer& ObjectInitializer);
+
+#pragma endregion
+
+#pragma region Reset
+
+public:
+	virtual void Reset() override;
 
 #pragma endregion
 
