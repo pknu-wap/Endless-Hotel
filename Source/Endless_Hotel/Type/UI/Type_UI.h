0d3628255901f414b2842b07f824c46f7868c217
이상혁 <imddongs@naver.com>
2026-09-07 21:49:05 +0900

    Feat: 수첩 확대 UI 추가

--- Source/Endless_Hotel/Type/UI/Type_UI.h
+++ Source/Endless_Hotel/Type/UI/Type_UI.h
@@ -15,6 +15,7 @@ enum class EWidgetType : uint8
 	PopUp_Setting			UMETA(DisplayName = "PopUp_Setting"),
 	PopUp_Read				UMETA(DisplayName = "PopUp_Read"),
 	PopUp_NoteBook			UMETA(DisplayName = "PopUp_NoteBook"),
+	PopUp_NoteBookZoomIn	UMETA(DisplayName = "PopUp_NoteBookZoomIn"),
 	PopUp_ResetProgression	UMETA(DisplayName = "PopUp_ResetProgression"),
 	PopUp_ResetSetting		UMETA(DisplayName = "PopUp_ResetSetting"),
 	PopUp_MainMenuCheck		UMETA(DisplayName = "PopUp_MainMenuCheck"),
--- Source/Endless_Hotel/UI/PopUp/Read/UI_PopUp_NoteBookZoomIn.h
+++ Source/Endless_Hotel/UI/PopUp/Read/UI_PopUp_NoteBookZoomIn.h
@@ -4,13 +4,13 @@
 
 #include "UI/PopUp/UI_PopUp_Base.h"
 #include <CoreMinimal.h>
-#include <UI_PopUp_WrongCheck.generated.h>
+#include <UI_PopUp_NoteBookZoomIn.generated.h>
 
 UCLASS(Meta = (DisableNativeTick))
-class ENDLESS_HOTEL_API UUI_PopUp_WrongCheck : public UUI_PopUp_Base
+class ENDLESS_HOTEL_API UUI_PopUp_NoteBookZoomIn : public UUI_PopUp_Base
 {
 	GENERATED_BODY()
-
+	
 #pragma region Base
 
 protected:
@@ -18,22 +18,23 @@ protected:
 
 #pragma endregion
 
+#pragma region Description
+
+public:
+	void SetDescription(uint8 Index);
+
+private:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UImage> Image_Picture;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<class UTextBlock> Text_Description;
+
+#pragma endregion
+
 #pragma region Button
 
 private:
-	UFUNCTION()
-	void Click_Yes();
-
-	UFUNCTION()
-	void Click_No();
-
-private:
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<class UButton> Button_Yes;
-
-	UPROPERTY(meta = (BindWidget))
-	TObjectPtr<class UButton> Button_No;
-
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<class UButton> Button_ESC;
 
