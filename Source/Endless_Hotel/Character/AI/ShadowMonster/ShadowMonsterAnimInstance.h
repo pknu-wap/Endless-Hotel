3b76fafd5d35de6398b38fbd7870d23cb68cf961
kwkim526 <polops2331@gmail.com>
2026-09-16 23:40:50 +0900

    Feat: 액자에서 그림자
    - 액자에서 그림자 나오는 기능 추가

---
+++ Source/Endless_Hotel/Character/AI/ShadowMonster/ShadowMonsterAnimInstance.h
@@ -0,0 +1,17 @@
+﻿// Copyright by 2026-1 WAP Game 2 team
+
+#pragma once
+
+#include "Character/AI/BaseAIAnimInstance.h"
+#include <CoreMinimal.h>
+#include <ShadowMonsterAnimInstance.generated.h>
+
+UCLASS()
+class ENDLESS_HOTEL_API UShadowMonsterAnimInstance : public UBaseAIAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "State")
+	bool bMonsterAppear = false;
+};
